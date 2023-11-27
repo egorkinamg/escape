cb99c28bd90467da9f283b3b81836ab349f606a0
egorkinamg <117100558+egorkinamg@users.noreply.github.com>
2023-11-27 17:49:23 +0200

    add files

---
+++ main.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector(".burger")
+
+let menu = document.querySelector(".header ul")
+
+burger.addEventListener("click",function(){
+    menu.classList.toggle('test')
+})
